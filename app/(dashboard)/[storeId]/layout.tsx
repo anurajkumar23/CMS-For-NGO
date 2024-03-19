ffbf7ce9d86eb175cc954f7162a62add0b4b8cd4
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-03-19 19:37:49 +0530

    first commit

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,39 @@
+import { redirect } from 'next/navigation';
+import { auth, authMiddleware } from '@clerk/nextjs';
+import Navbar from '@/components/navbar'
+import prismadb from '@/lib/prismadb';
+
+export default async function DashboardLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode
+  params: { storeId: string }
+}) {
+  
+  await authMiddleware();
+    
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const store = await prismadb.store.findFirst({ 
+    where: {
+      id: params.storeId,
+      userId,
+    }
+   });
+
+  if (!store) {
+    redirect('/');
+  };
+
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
